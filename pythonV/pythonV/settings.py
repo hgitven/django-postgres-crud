26263ec0be821817dcddd1d3f34612e191393e82
hgitven <161238269+hgitven@users.noreply.github.com>
2026-03-16 13:58:42 -0400

    Change database password to empty string
    Updated the database password to an empty string.

--- pythonV/pythonV/settings.py
+++ pythonV/pythonV/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "pv",
         "USER": "postgres",
-        "PASSWORD": "postgre1.",
+        "PASSWORD": "",  #Su contraseña
         "HOST": "127.0.0.1",
         "PORT": "5432",
         
